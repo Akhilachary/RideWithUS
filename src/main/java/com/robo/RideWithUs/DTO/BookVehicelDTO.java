73417c84339963de3b97fec0319eac3f4655931b
= <=>
2025-12-26 14:21:37 +0530

    Resolved merge conflict in BookVehicelDTO

--- src/main/java/com/robo/RideWithUs/DTO/BookVehicelDTO.java
+++ src/main/java/com/robo/RideWithUs/DTO/BookVehicelDTO.java
@@ -1,98 +1,139 @@
 package com.robo.RideWithUs.DTO;
 
-<<<<<<< HEAD
 import com.robo.RideWithUs.Entity.Vehicle;
 
 public class BookVehicelDTO {
 
-	private long customerMobileNumber;
-	private long driverMobileNumber;
-	private double longitude;
-	private double latitude;
-	private Vehicle vehicle;
-	
-	public Vehicle getVehicle() {
-		return vehicle;
-	}
-	public void setVehicle(Vehicle vehicle) {
-		this.vehicle = vehicle;
-	}
-	public long getCustomerMobileNumber() {
-		return customerMobileNumber;
-=======
+    // Customer & Driver info
+    private long customerMobileNumber;
+    private long driverMobileNumber;
 
-public class BookVehicelDTO {
+    // Location info
+    private double longitude;
+    private double latitude;
+    private String sourceLocation;
+    private String destinationLocation;
 
-	private int vehicleID;
-	private String sourceLocation;
-	private String destinationLocation;
-	private int fare;
-	private int estiamtedTime;
-	private int distanceTravelled;
-	
-	
-	public int getVehicleID() {
-		return vehicleID;
->>>>>>> main
-	}
-	public void setVehicleID(int vehicleID) {
-		this.vehicleID = vehicleID;
-	}
-	public String getSourceLocation() {
-		return sourceLocation;
-	}
-	public void setSourceLocation(String sourceLocation) {
-		this.sourceLocation = sourceLocation;
-	}
-	public String getDestinationLocation() {
-		return destinationLocation;
-	}
-	public void setDestinationLocation(String destinationLocation) {
-		this.destinationLocation = destinationLocation;
-	}
-	public int getFare() {
-		return fare;
-	}
-	public void setFare(int fare) {
-		this.fare = fare;
-	}
-<<<<<<< HEAD
-	
-	public BookVehicelDTO(long customerMobileNumber, long driverMobileNumber, double longitude, double latitude,
-			Vehicle vehicle) {
-		super();
-		this.customerMobileNumber = customerMobileNumber;
-		this.driverMobileNumber = driverMobileNumber;
-		this.longitude = longitude;
-		this.latitude = latitude;
-		this.vehicle = vehicle;
-=======
-	public int getEstiamtedTime() {
-		return estiamtedTime;
-	}
-	public void setEstiamtedTime(int estiamtedTime) {
-		this.estiamtedTime = estiamtedTime;
-	}
-	public int getDistanceTravelled() {
-		return distanceTravelled;
-	}
-	public void setDistanceTravelled(int distanceTravelled) {
-		this.distanceTravelled = distanceTravelled;
-	}
-	
-	public BookVehicelDTO(int vehicleID, String sourceLocation, String destinationLocation, int fare, int estiamtedTime, int distanceTravelled) {
-		super();
-		this.vehicleID = vehicleID;
-		this.sourceLocation = sourceLocation;
-		this.destinationLocation = destinationLocation;
-		this.fare = fare;
-		this.estiamtedTime = estiamtedTime;
-		this.distanceTravelled = distanceTravelled;
->>>>>>> main
-	}
-	public BookVehicelDTO() {
-		super();
-	}
+    // Trip details
+    private int fare;
+    private int estimatedTime;
+    private int distanceTravelled;
 
-		
+    // Vehicle info
+    private int vehicleID;
+    private Vehicle vehicle;
+
+    // Constructors
+    public BookVehicelDTO() {
+        super();
+    }
+
+    public BookVehicelDTO(long customerMobileNumber, long driverMobileNumber,
+                          double longitude, double latitude,
+                          int vehicleID, String sourceLocation,
+                          String destinationLocation, int fare,
+                          int estimatedTime, int distanceTravelled,
+                          Vehicle vehicle) {
+
+        this.customerMobileNumber = customerMobileNumber;
+        this.driverMobileNumber = driverMobileNumber;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.vehicleID = vehicleID;
+        this.sourceLocation = sourceLocation;
+        this.destinationLocation = destinationLocation;
+        this.fare = fare;
+        this.estimatedTime = estimatedTime;
+        this.distanceTravelled = distanceTravelled;
+        this.vehicle = vehicle;
+    }
+
+    // Getters & Setters
+    public long getCustomerMobileNumber() {
+        return customerMobileNumber;
+    }
+
+    public void setCustomerMobileNumber(long customerMobileNumber) {
+        this.customerMobileNumber = customerMobileNumber;
+    }
+
+    public long getDriverMobileNumber() {
+        return driverMobileNumber;
+    }
+
+    public void setDriverMobileNumber(long driverMobileNumber) {
+        this.driverMobileNumber = driverMobileNumber;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getSourceLocation() {
+        return sourceLocation;
+    }
+
+    public void setSourceLocation(String sourceLocation) {
+        this.sourceLocation = sourceLocation;
+    }
+
+    public String getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public void setDestinationLocation(String destinationLocation) {
+        this.destinationLocation = destinationLocation;
+    }
+
+    public int getFare() {
+        return fare;
+    }
+
+    public void setFare(int fare) {
+        this.fare = fare;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public int getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public void setDistanceTravelled(int distanceTravelled) {
+        this.distanceTravelled = distanceTravelled;
+    }
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(int vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
